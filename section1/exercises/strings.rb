25faa2048bc009e569b207ab400d024ad15f2ad9
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-07 16:42:31 -0600

    Initial commit

--- section1/exercises/strings.rb
+++ section1/exercises/strings.rb
@@ -1,15 +1,13 @@
-# In the below exercises, write code that achieves
+#In the below exercises, write code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your terminal:
 # `ruby section1/exercises/strings.rb`
 
-# Example: Write code that prints your name to the terminal:
-p "Alan Turing"
-
 # Write code that prints `Welcome to Turing!` to the terminal:
-p #YOUR CODE HERE
+p "Welcome to Turing!"
 
 # Write code that prints `99 bottles of pop on the wall...` to the terminal:
-# YOUR CODE HERE
+p "99 bottles of pop on the wall..."
 
 # Write out code to log one line from your favorite song or movie.
+p "HARRY! DID YOU PUT YOUR NAME INTO THE GOBLET OF FIRE?!"
