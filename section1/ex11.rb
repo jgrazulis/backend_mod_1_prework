332784422a3da230a6f74d2995fe9de8905e8410
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-07 15:20:16 -0600

    Finish exercises

---
+++ section1/ex11.rb
@@ -0,0 +1,15 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What is your favorite food? "
+favorite_food = gets.chomp
+print "What is your favorite movie"
+favorite_movie = gets.chomp
+
+puts "Your favorite food is #{favorite_food} and your favorite movie is #{favorite_movie}!"
