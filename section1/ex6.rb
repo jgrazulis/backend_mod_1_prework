34016380e86aa554b6ea44acc9e90e649fedfcb3
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-07 14:38:36 -0600

    Add study drill prompts

---
+++ section1/ex6.rb
@@ -0,0 +1,35 @@
+#define variable
+types_of_people = 10
+#define x varible using types of people
+x = "There are #{types_of_people} types of people."
+#define binary
+binary = "binary"
+#define do_not varaible
+do_not = "don't"
+#Use binary and do_not to create new variable y
+y = "Those who know #{binary} and those who #{do_not}."
+#print x
+puts x
+#print y
+puts y
+#print sentence and x variable
+puts "I said: #{x}."
+#print y variable
+puts "I also said: '#{y}'."
+
+#use boolean to define hilarious
+hilarious = false
+#define variable joke_evaluation using hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#print joke_evaluation
+puts joke_evaluation
+#declare w
+w = "This is the left side of..."
+#declare e
+e = "a string with a right side."
+#print w and e
+puts w + e
+
+#String is put inside a string 6 times including x and y
+#Adding w and e makes a longer string because w and e are both strings
+#No, they do not work because a single quote does not acknowledge string interpolation.
