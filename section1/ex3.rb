5ddf1c3657611a2f72dde2c4c5991ea95d6e3364
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-07 14:34:20 -0600

    Add floating point numbers

---
+++ section1/ex3.rb
@@ -0,0 +1,28 @@
+#print and explain purpose of code
+puts "I will now count my chickens:"
+#print formula for determining the number of Hens; adding 25 and 30 divided by 6.
+puts "Hens #{25.0 + 30 / 6}"
+#print the formula for determining the number of Roosters; subtracting 25 from 100 multiplied by 3 percentage with 4 remaining.
+puts "Roosters #{100 - 25 * 3 % 4}"
+#print action of counting the eggs
+puts "Now I will count the eggs:"
+#print formula for counting eggs: adding 3, 2, 1, subtracting 5, adding 4 with the percentage with 2 remaining - 1 divided by 4 + 6.
+puts 3.0 + 2.5 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#print string
+puts "Is it true that 3.0 + 2.5 < 5 - 7?"
+#print equation
+puts 3 + 2 < 5 - 7
+#print question and math equation to declare as true or false
+puts "What is 3.0 + 2.5? #{3.0 + 2.5}"
+#print question and math equation to declare as true or false
+puts "What is 5.5 - 7? #{5.5 - 7}"
+#print string
+puts "Oh, that's why it's false."
+#print string
+puts "How about some more."
+#print string and equation to determine if 5 is greater than -2
+puts "Is it greater? #{5.5 > -2}"
+#print string and equation to determine if 5 is greater than or equal to -2
+puts "Is it greater or equal? #{5.5 >= -2}"
+#print string and equation to determine if 5 is less than or equal to -2.
+puts "Is it less or equal? #{5.5 <= -2}"
---
+++ section1/ex3_a.rb
@@ -0,0 +1,2 @@
+puts 3 * 9 / 2
+puts "The answer is #{{3 * 9 / 2}}"
