4c87ff0cf7ab125cf717e95f61bba29e4c5bcf03
Jessica Grazulis <jlgrazulis@gmail.com>
2021-09-08 10:28:15 -0600

    Add section 1

---
+++ section1/exercises/reflection.md
@@ -0,0 +1,32 @@
+
+
+    How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
+
+    Keep reading: I do this already since it helps me to best learn and retain information.
+    Eat healthy: I'm trying to do this more often since it does help me to manage stress, anxiety and depression along with exercise.
+    Take poms: I am definitely getting better at taking small breaks!
+
+    How would you print the string "Hello World!" to the terminal?
+    p "Hello World!"
+
+    What character is used to indicate comments in a ruby file?
+    #
+
+    Explain the difference between an integer and a float?
+    A float includes a decimal point while an integer is a whole value.
+
+    In the space below, create a variable animal that holds the string "zebra"
+    animal = "zebra"
+
+    How would you print the string "zebra" using the variable that you created above?
+    p animal
+
+    What is interpolation? Use interpolation to print a sentence using the variable animal.
+    Interpolation is including multiple variables in an output that are replaces with their respective values.
+    p "The #{animal} has black and white stripes which unfortunately may make the #{animal} desirable for fashion designers"
+
+    What method is used to get input from a user?
+    gets.chomp
+
+    Name and describe two common string methods:
+    .length, .split
--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,37 +11,41 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
-# Write code that saves the string 'Harry Potter must not return to Hogwarts!'
-# and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+says = 'Harry Potter must not return to Hogwarts!'
+p says
+
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students = 22 + 2
 p students
 
-# Write code that subracts 2 from the `students` variable and
-# prints the result:
-# YOUR CODE HERE
+students = 22 - 2
 p students
 
 
-# YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
-# Store the appropriate data types in each.
-# print all three variables to the terminal.
+first_name = "Mia"
+is_hungry = "Mia always"
+number_of_pets = 1
+p first_name, is_hungry, number_of_pets
 
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
+#I decided to use strings for the first and second variables because I was thinking of my dog's name. For the third variable, "number" is included in the name so I used an integer.
 
 # Explain.
 
 
 # YOU DO:
-# Re-assign the values to the three variables from the previous challenge to different values (but same data type).
+first_name = "Jessica"
+is_hungry = "Mia"
+number_of_pets = 3
+p first_name
+p is_hungry
+p number_of_pets
 # print all three variables to the terminal.
 
 
@@ -49,11 +53,13 @@ p students
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
+p healthy_snacks
+p junk_food_snacks
 
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
